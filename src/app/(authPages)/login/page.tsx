90cadc16483148d6258fc08c2c605a7d9c8561c4
Aniket Sahoo <aniketsahoo758@gmail.com>
2023-10-31 22:14:02 +0530

    redirect false

--- src/app/(authPages)/login/page.tsx
+++ src/app/(authPages)/login/page.tsx
@@ -43,7 +43,7 @@ export default function Login() {
             email: authState.email,
             password: authState.password,
             callbackUrl: "/",
-            redirect: true,
+            redirect: false,
           });
         } else if (response.status == 400) {
           setErrors(response.errors);
